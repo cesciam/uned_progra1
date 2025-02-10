2f72812af458a975f96fe100bb1be04fe6352885
csclaptop <csc8848@gmail.com>
2025-02-10 06:58:28 -0600

    Listado e inicio de entrenamiento

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -64,8 +64,6 @@ string verifica_id_pokemon(){
             }
         }
     }
-
-
     return id_ingresado;
 }
 
@@ -103,12 +101,73 @@ void registrar_pokemon(){
 
 }
 
+void listar_pokemones(){
+
+    if(ids_pokemon.size() > 0){
+        cout << "=============================" << endl;
+        cout << "EQUIPO POKEMON" << endl;
+        cout << "=============================" << endl;
+
+        for (int i = 0; i < ids_pokemon.size(); i++){
+            cout << "- Nombre: " << nombres_pokemon[i] <<endl;
+            cout << "ID: " << ids_pokemon[i] <<endl;
+            cout << "Tipo: " << tipos_pokemon[i] <<endl;
+            cout << "Nivel de poder: " << poderes_pokemon[i] <<endl;
+            cout << "-------------------------" << endl;
+        }
+    } else {
+        cout << "No hay Pokemon registrados aun." << endl;
+
+    }
+}
+
+int buscar_pokemon(string id){
+    for(int i = 0; i < ids_pokemon.size(); i++){
+        if(ids_pokemon[i] == id){
+            return i;
+        } else {
+            return -1;
+        }
+    }
+}
+
+void verifica_tipo_entrenamiento(){
+    bool continuar = true;
+    int tipo;
+    while(continuar){
+        cout << "(1. Combateen gimnasio." << endl;
+        cout << "2. Batalla con otro entrenador." << endl;
+        cout << "3. Practica de habilidades." << endl;
+        cout << "Ingrese el tipo de entrenamiento (1-3): ";
+        cin >> tipo;
+
+        if (tipo>3 || tipo <1 ){
+            cout << "Seleccione una opcion correcta (1-3)." << endl;
+        } else{
+            continuar = false;
+        }
+    }
+}
+
+
+void entrenar_pokemon(){
+    cout << "Ingrese el identificador del pokemon: ";
+    cin >> id_ingresado;
+
+    int posicion_pokemon = buscar_pokemon(id_ingresado);
+
+    if (posicion_pokemon > 0){
+        verifica_tipo_entrenamiento();
+
+    } else {
+        cout << "No existe un pokemon con el ID ingresado." << endl;
+    }
+}
+
 void menu(){
     bool continuar = true;
     int opcion_ingresada = 0;
-
     while(continuar){
-
         cout << "=============================" << endl;
         cout << "SISTEMA DE ENTRENAMIENTO POKEMON" << endl;
         cout << "1. Registrar un Pokemon" << endl;
@@ -125,11 +184,11 @@ void menu(){
                 break;
             }
             case 2:{
-                //registrar_pokemon();
+                entrenar_pokemon();
                 break;
             }
             case 3:{
-                //registrar_pokemon();
+                listar_pokemones();
                 break;
             }
             case 4:{
