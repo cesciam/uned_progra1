2a487ad5b5830885c10b47254f40d3fbcc671104
CSCPC <csc8848@gmail.com>
2025-02-11 22:39:02 -0600

    Entrenar y busqueda del mas fuerte terminado. Faltan validaciones de tipo INT

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -28,30 +28,35 @@ int verifica_tipo_pokemon(){
     return tipo;
 }
 
+bool es_numerico(string numero){
+    try{
+        //string to int
+        stoi(numero);
+        return true;
+    } catch(const std::invalid_argument&){
+        //Este error salta cuando el string ingresado no es un int válido
+        return false;
+    }
+}
+
 string verifica_id_pokemon(){
     bool continuar = true;
-    bool esNumerico = false;
     string id_ingresado;
     while(continuar){
         cout << "Ingrese el identificador del pokemon: ";
         cin >> id_ingresado;
         // Se verifica que lo ingresado sea de tipo numerico
-        try{
-            //string to int
-            stoi(id_ingresado);
-            esNumerico = true;
-        } catch(const std::invalid_argument&){
-            //Este error salta cuando lo ingresado no es de tipo string
+        if(es_numerico(id_ingresado)){
+           if (id_ingresado.length() != 6){
+                cout << "El ID debe tener solamente 6 digitos." << endl;
+                continuar = true;
+            } else {
+                continuar = false;
+            }
+        } else{
             cout << "El ID debe tener solamente digitos numericos." << endl;
-            esNumerico = false;
-        }
-        if (id_ingresado.length() != 6){
-            cout << "El ID debe tener solamente 6 digitos." << endl;
             continuar = true;
-        } else if(esNumerico){
-            continuar = false;
         }
-
         if(!continuar){
             //Este ciclo verifica que el ID no haya sido registrado anteriormente
             for (string i : ids_pokemon){
@@ -101,9 +106,12 @@ void registrar_pokemon(){
 
 }
 
-void listar_pokemones(){
+bool existen_pokemones(){
+    return (ids_pokemon.size() > 0) ? true : false;
+}
 
-    if(ids_pokemon.size() > 0){
+void listar_pokemones(){
+    if(existen_pokemones){
         cout << "=============================" << endl;
         cout << "EQUIPO POKEMON" << endl;
         cout << "=============================" << endl;
@@ -117,25 +125,25 @@ void listar_pokemones(){
         }
     } else {
         cout << "No hay Pokemon registrados aun." << endl;
-
     }
 }
 
 int buscar_pokemon(string id){
+
     for(int i = 0; i < ids_pokemon.size(); i++){
+        cout << ids_pokemon[i] << endl;
         if(ids_pokemon[i] == id){
             return i;
-        } else {
-            return -1;
         }
     }
+    return -1;
 }
 
 void verifica_tipo_entrenamiento(){
     bool continuar = true;
     int tipo;
     while(continuar){
-        cout << "(1. Combateen gimnasio." << endl;
+        cout << "1. Combateen gimnasio." << endl;
         cout << "2. Batalla con otro entrenador." << endl;
         cout << "3. Practica de habilidades." << endl;
         cout << "Ingrese el tipo de entrenamiento (1-3): ";
@@ -149,18 +157,64 @@ void verifica_tipo_entrenamiento(){
     }
 }
 
+int verifica_dificultad(){
+    bool continuar = true;
+    int dificultad;
+    while(continuar){
+        cout << "Ingrese la dificultad del entrenamiento: ";
+        cin >> dificultad;
+
+        if (dificultad>100 || dificultad <1 ){
+            cout << "Ingrese una dificultad correcta (1-100)." << endl;
+        } else{
+            continuar = false;
+        }
+    }
+    return dificultad;
+}
+
 
 void entrenar_pokemon(){
+    string id_ingresado;
     cout << "Ingrese el identificador del pokemon: ";
     cin >> id_ingresado;
-
     int posicion_pokemon = buscar_pokemon(id_ingresado);
 
-    if (posicion_pokemon > 0){
+    if (posicion_pokemon > -1){
         verifica_tipo_entrenamiento();
-
+        if(poderes_pokemon[posicion_pokemon] >= verifica_dificultad()){
+            cout << "El entrenamiento ha sido exitoso!" << endl;
+            poderes_pokemon[posicion_pokemon] = poderes_pokemon[posicion_pokemon] + 10;
+        } else {
+            cout << "El entrenamiento ha fallado!" << endl;
+            cout << "Cada derrota es una leccion. ¡La proxima vez seros aun mejor!" << endl;
+        }
     } else {
         cout << "No existe un pokemon con el ID ingresado." << endl;
+    }
+}
+
+void pokemon_fuerte(){
+    if(existen_pokemones){
+        int poder_mas_fuerte = -1;
+        int posicion_mas_fuerte = -1;
+        for (int i = 0; i < poderes_pokemon.size(); i++){
+            if(poderes_pokemon[i] > poder_mas_fuerte){
+                posicion_mas_fuerte = i;
+                poder_mas_fuerte = poderes_pokemon[i];
+            }
+        }
+        cout << "=============================" << endl;
+        cout << "POKEMON MAS FUERTE" << endl;
+        cout << "=============================" << endl;
+        cout << "- Nombre: " << nombres_pokemon[posicion_mas_fuerte] <<endl;
+        cout << "ID: " << ids_pokemon[posicion_mas_fuerte] <<endl;
+        cout << "Tipo: " << tipos_pokemon[posicion_mas_fuerte] <<endl;
+        cout << "Nivel de poder: " << poderes_pokemon[posicion_mas_fuerte] <<endl;
+        cout << "-------------------------" << endl;
+
+    } else {
+        cout << "No hay Pokemon registrados aun." << endl;
     }
 }
 
@@ -192,7 +246,7 @@ void menu(){
                 break;
             }
             case 4:{
-                //registrar_pokemon();
+                pokemon_fuerte();
                 break;
             }
             case 5:{
@@ -200,12 +254,30 @@ void menu(){
                 cout << "Gracias por usar nuestro sistema Pokemon!" << endl;
                 break;
             }
+            default:{
+                cout << "Ingrese una opcion correcta. (1-5)" << endl;
+            }
         }
+    }
+}
+
+void llenar_pokemones() {
+    vector<string> ids = {"000001", "000002", "000003", "000004", "000005"};
+    vector<string> nombres = {"Pikachu", "Charmander", "Bulbasaur", "Squirtle", "Eevee"};
+    vector<int> tipos = {4, 1, 3, 2, 3};  // 1 = Fuego, 2 = Agua, 3 = Planta, 4 = Eléctrico
+    vector<int> poderes = {80, 75, 70, 85, 90};
+
+    for (int i = 0; i < 5; i++) {
+        ids_pokemon.push_back(ids[i]);
+        nombres_pokemon.push_back(nombres[i]);
+        tipos_pokemon.push_back(tipos[i]);
+        poderes_pokemon.push_back(poderes[i]);
     }
 }
 
 int main()
 {
+    llenar_pokemones();
     menu();
     return 0;
 }
